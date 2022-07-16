f509289241a18f6c6390cdbc65ede4f43a4307b6
d3xdll <sauerhonig@gmail.com>
2022-07-16 17:31:03 +0300

    Make main functions, creates given number of divs, changes on mouseover

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
 const mainSquare=document.querySelector('#mainBoard');
 
-const squares=document.createElement('div');
+
+const createBtn=document.querySelector('button');
+console.log(createBtn);
+createBtn.addEventListener('click', ()=>{
+  let divsCount=document.querySelector('input').value;
+  createDesk(divsCount)});
 
 
-const butnSmall = document.querySelector('#btnSmall');
-butnSmall.addEventListener('click', ()=> createDesk(267));
 
+function checkDivs(){
+  const check=document.querySelectorAll('div.coloredDivs');
+  return check;
+}
 
-const butnMedium = document.querySelector('#btnMedium');
-butnMedium.addEventListener('click', ()=> createDesk(512));
-
-
-const butnBig = document.querySelector('#btnBig');
-butnBig.addEventListener('click', ()=> createDesk(768));
-
-
-function createDesk(numberDivs){
-  for (let i=1; i<numberDivs; i++){
+function createDivs(divsCount){
+  for (let i=0; i<divsCount; i++){
     const squares=document.createElement('div');
+    squares.classList.add('coloredDivs');
     squares.style.height="20px";
     squares.style.width="20px";
-    squares.style.backgroundColor="blue";
+    squares.style.backgroundColor="grey";
     squares.style.border="1px solid black";
-    squares.style.margin="2px";
     mainSquare.appendChild(squares);}
+    change();
+}
+
+function createDesk(divsCount){
+  if (checkDivs().length!=0){
+    checkDivs().forEach((element) =>{
+      element.remove()})
   }
+  createDivs(divsCount);
+}
+
+  function randomColor(){
+    return ("#" + ("00000" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16)).slice(-6));
+  }
+  
+  function selectDivs(){
+    const hoverDivs=document.querySelectorAll('div.coloredDivs');
+    return hoverDivs;
+  }
+  
+  function change(){
+    selectDivs().forEach((element) =>{
+      element.addEventListener('mouseover', ()=>
+      element.style.backgroundColor=randomColor());
+    });
+  }
+
+
+  const propHover=document.getElementById('prop');
+  propHover.addEventListener('mouseover', ()=>propHover.style.backgroundColor=randomColor());

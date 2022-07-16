517c727642bd69917aba58d9da16f9ffd7052a04
d3xdll <sauerhonig@gmail.com>
2022-07-16 23:00:24 +0300

    Add pixel grid functionality

--- script.js
+++ script.js
@@ -8,24 +8,34 @@ createBtn.addEventListener('click', ()=>{
   createDesk(divsCount)});
 
 
-
+// checks nodelist
 function checkDivs(){
   const check=document.querySelectorAll('div.coloredDivs');
   return check;
 }
 
+// creates given number of divs
 function createDivs(divsCount){
+  let pixel=500/divsCount;
   for (let i=0; i<divsCount; i++){
-    const squares=document.createElement('div');
-    squares.classList.add('coloredDivs');
-    squares.style.height="20px";
-    squares.style.width="20px";
-    squares.style.backgroundColor="grey";
-    squares.style.border="1px solid black";
-    mainSquare.appendChild(squares);}
+    const flexDivs=document.createElement('div');
+    flexDivs.classList.add('flexColums');
+    flexDivs.style.display="flex";
+    flexDivs.style.flexDirection="column"
+    mainSquare.appendChild(flexDivs);
+    for (let m=0; m<divsCount; m++){
+      const squares=document.createElement('div');
+      squares.classList.add('coloredDivs');
+      squares.style.height=`${pixel}px`;
+      squares.style.width=`${pixel}px`;
+      squares.style.backgroundColor="grey";
+      squares.style.border="0.5px solid black";
+      flexDivs.appendChild(squares);}
+    }
     change();
 }
 
+// checks if some divs is already on a board, if true > removes and creates given number
 function createDesk(divsCount){
   if (checkDivs().length!=0){
     checkDivs().forEach((element) =>{
@@ -34,22 +44,20 @@ function createDesk(divsCount){
   createDivs(divsCount);
 }
 
+// creates random color
   function randomColor(){
     return ("#" + ("00000" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16)).slice(-6));
   }
-  
+
+// makes divs change style 
   function selectDivs(){
     const hoverDivs=document.querySelectorAll('div.coloredDivs');
     return hoverDivs;
   }
-  
+
   function change(){
     selectDivs().forEach((element) =>{
       element.addEventListener('mouseover', ()=>
       element.style.backgroundColor=randomColor());
     });
   }
-
-
-  const propHover=document.getElementById('prop');
-  propHover.addEventListener('mouseover', ()=>propHover.style.backgroundColor=randomColor());
